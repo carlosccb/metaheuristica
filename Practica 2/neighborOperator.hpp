f02dab9dbd947d11bdd0c361124e5a739d953886
kindarnakes <kindenene@gmail.com>
2016-03-28 13:09:59 +0200

    Update neighborOperator.hpp

--- Practica 2/neighborOperator.hpp
+++ Practica 2/neighborOperator.hpp
@@ -33,10 +33,7 @@ class neighborOperator{
 		//Metodo que genera un vecino de una solucion, teniendo en cuenta unos parametros
 		Solution generateNeighbor(Solucion &initialSolution, const int &parametros){
 
-			
-
-				//Ni idea de que hay que hacer con los parametros o de cuales son
-				//Pero el resto es solo implementar la funcionalidad de ese operador
+		
 				
 
 
