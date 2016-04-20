e731b91d8ba93f2abbbc092dac8001e1bf3ffbe8
Ivan Bonaque Muñoz <i22bomui@uco.es>
2016-04-20 11:11:00 +0200

    Añadido constructor vacio

--- Practica3/tsp/SolucionViajante.hpp
+++ Practica3/tsp/SolucionViajante.hpp
@@ -83,7 +83,7 @@ class SolucionViajante{
    
    inline void setFitness(double fitness){_fitness = fitness;};
      
-     
+  SolucionViajante(){};
   SolucionViajante(unsigned int objetos=0){
     for (unsigned int i=0; i< objetos; i++)
     _solucion.push_back(i);
